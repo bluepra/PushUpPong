ded1a06acc8fa1793f9442601a2df9600c54a474
bluepra <parora9@wisc.edu>
2023-03-25 23:45:44 -0500

    HomePage coming along

--- push-up-pong/src/pages/HomePage.jsx
+++ push-up-pong/src/pages/HomePage.jsx
@@ -1,3 +1,22 @@
+// import { useNavigate } from 'react-router-dom';
+
+// function HomePage() {
+//     const navigate = useNavigate();
+
+//     function handleButtonClick() {
+//         navigate('/game');
+//     }
+
+//     return (
+//         <div>
+//             <h1>PUSH UP PONG</h1>
+//             <button onClick={handleButtonClick}>Play Game</button>
+//         </div>
+//     );
+// }
+
+// export default HomePage;
+
 import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
@@ -7,10 +26,56 @@ function HomePage() {
         navigate('/game');
     }
 
+    function generateRandomString() {
+        let result = '';
+        const characters =
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        for (let i = 0; i < 6; i++) {
+            result += characters.charAt(
+                Math.floor(Math.random() * charactersLength)
+            );
+        }
+        return result;
+    }
+
     return (
-        <div>
-            <h1>PUSH UP PONG</h1>
-            <button onClick={handleButtonClick}>Play Game</button>
+        // <button onClick={handleButtonClick}>Create Game</button>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                height: '100vh',
+            }}
+        >
+            <h1>Push Up Pong</h1>
+            <div>
+                <button onClick={handleButtonClick}>Create Game</button>
+            </div>
+            <div>
+                <p>OR</p>
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                }}
+            >
+                <p style={{ textAlign: 'center' }}>Enter Code to Join</p>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <input type="text" placeholder="Code" id="code" />
+                    <button>Join</button>
+                </div>
+            </div>
         </div>
     );
 }
